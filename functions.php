5e080984431e1b5322dd481f42e5e07ced820539
Dwayne Charrington <dwayencharrington@gmail.com>
2013-09-17 12:11:09 +1000

    Load styling and scripts in functions.php

--- functions.php
+++ functions.php
@@ -97,17 +97,20 @@ add_action( 'widgets_init', 'theme_widgets_init' );
  */
 function theme_scripts() {
 	wp_enqueue_style( 'theme-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'theme-mainstyle', get_template_directory_uri() . '/css/theme.css' );
 
-	wp_enqueue_script( 'theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), null, true );
 
-	wp_enqueue_script( 'theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), null, true );
+
+	wp_enqueue_script( 'theme-mainscript', get_template_directory_uri() . '/js/theme.js', array('jquery'), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( 'theme-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+		wp_enqueue_script( 'theme-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), null, true );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'theme_scripts' );
